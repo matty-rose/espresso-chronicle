109ceda0d0c27b6d35cd8b44cd10e3c3b3910aaa
Matthew Rose <matthewrose153@gmail.com>
2022-03-22 19:40:40 +1000

    feat(infra): add firestore db

--- infra/main.go
+++ infra/main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/storage"
+	"fmt"
+
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/appengine"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
+
+const (
+	ProjectId   string = "espressolog"
+	ProjectName string = "espresso-chronicle"
+)
+
+func formatName(ctx *pulumi.Context, thing string) string {
+	return fmt.Sprintf("%s-%s-%s", ProjectName, ctx.Stack(), thing)
+}
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create a GCP resource (Storage Bucket)
-		bucket, err := storage.NewBucket(ctx, "my-bucket", &storage.BucketArgs{
-			Location: pulumi.String("US"),
+		// Get stack specific configuration
+		config := config.New(ctx, ProjectName)
+
+		// Create the Firestore database
+		_, err := appengine.NewApplication(ctx, formatName(ctx, "db"), &appengine.ApplicationArgs{
+			Project:      pulumi.String(ProjectId),
+			LocationId:   pulumi.String(config.Require("region")),
+			DatabaseType: pulumi.String("CLOUD_FIRESTORE"),
 		})
 		if err != nil {
 			return err
 		}
 
-		// Export the DNS name of the bucket
-		ctx.Export("bucketName", bucket.Url)
 		return nil
 	})
 }
